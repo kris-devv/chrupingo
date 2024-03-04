9670685a16a2914d6d592393535e1308869c5b43
Cocolak <krystian.figiela000@gmail.com>
2024-03-04 14:12:59 +0100

    Update visual

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_index.css">
-    <link rel="icon" href="chrupek.png">
+    <link rel="stylesheet" href="css/style_index.css">
+    <link rel="icon" href="img/chrupek.png">
+    <meta charset="UTF-8">
+    <meta name="description" content="CHRUPINGO BINGO!">
+    <meta name="keywords" content="bingo, chrupingo, chrupcala">
+    <meta name="author" content="Krystian Figiela, Patryk Lipiński">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Madimi+One&display=swap" rel="stylesheet">
@@ -21,6 +26,12 @@
         <p class="error"> <?php echo $_GET["error"]; ?> </p>
     <?php } ?>
     </form>
+    <p id="wersja">V.0.1</p>
+    <div id="githubs">
+        <a href="githubs.html">
+            <img src="img/github-mark.png" alt="Github Link" height="100px" width="100px">
+        </a>
+    </div>
     </main>
 </body>
 </html>
